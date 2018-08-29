3affacdf23dbb2ded699b784eb6259178399e49b
xuechenwang <xuechenwang@cienet.com.cn>
2018-08-29 11:18:26 +0800

    modify parameter properties

--- src/main/kotlin/com/cienet/npdapp/rpcclient/RPCClient.kt
+++ src/main/kotlin/com/cienet/npdapp/rpcclient/RPCClient.kt
@@ -3,6 +3,7 @@ package com.cienet.npdapp.rpcclient
 import com.cienet.npdapp.number.*
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 
@@ -71,20 +72,25 @@ private class RPCClient: CliktCommand() {
     val address: String by option(help="Address of RPC").default("127.0.0.1:10005")
     val username: String by option(help="The user to login").default("user1")
     val password: String by option(help="The password").default("test")
-    val flowName: String by option(help="The Flow Name (query/access/transfer)").default("query")
-    val number: String by option(help="The number for the flow action").default("18612345678")
-    val partyName: String by option(help="The CordaX500Name of the party for the flow action").default("CTCC")
+    val flowName: String by argument(help="The Flow Name [query|access|transfer]")
+    val number: String by argument(help="The number for the flow action, e.g. 18612345678")
+    val partyName: String by option(help="The CordaX500Name of the party for the flow action").default("O=CTCC, L=Beijing, C=CN")
 
     override fun run() {
         connect(address, username, password)
 
         when (flowName) {
+            // --address 10.10.11.111:10006 --flow-name query --number 18612345678
             "query" -> {
                 logState(queryStateBy(number))
             }
+            // CMCC
+            // --address 10.10.11.111:10006 --flow-name access --number 18612345678
             "access" -> {
                 logState(performAccessFlow(number))
             }
+            // CUCC
+            // --address 10.10.11.111:10009 --flow-name transfer --number 18612345678 --party-name "O=CMCC, L=Beijing, C=CN"
             "transfer" -> {
                 logState(performTransferFromFlow(number, CordaX500Name.parse(partyName)))
             }
